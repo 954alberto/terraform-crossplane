6af5c03bbfbd7733249122306d5f9fc62b3cd9e9
Alberto Rodrguez Garcia <agarcia@schubergphilis.com>
2021-03-30 10:32:40 +0200

    WIP

--- variables.tf
+++ variables.tf
@@ -11,46 +11,6 @@ variable "map_accounts" {
     "888888888888",
   ]
 }
-
-# variable "map_roles" {
-#   description = "Additional IAM roles to add to the aws-auth configmap."
-#   type = list(object({
-#     rolearn  = string
-#     username = string
-#     groups   = list(string)
-#   }))
-
-#   default = [
-#     {
-#       rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/role1"
-#       username = "role1"
-#       groups   = ["system:masters"]
-#     },
-#   ]
-# }
-
-# variable "map_users" {
-#   description = "Additional IAM users to add to the aws-auth configmap."
-#   type = list(object({
-#     userarn  = string
-#     username = string
-#     groups   = list(string)
-#   }))
-
-#   default = [
-#     {
-#       userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user1"
-#       username = "user1"
-#       groups   = ["system:masters"]
-#     },
-#     {
-#       userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user2"
-#       username = "user2"
-#       groups   = ["system:masters"]
-#     },
-#   ]
-# }
-
 locals {
   map_users = [
     {
@@ -67,6 +27,11 @@ locals {
 }
 locals {
   map_roles = [
+    {
+      rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/role1"
+      username = "role1"
+      groups   = ["system:masters"]
+    },
     {
       rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/role1"
       username = "role1"
